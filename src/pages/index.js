009ec81d63e3ee59919d0871115c44c66f1a069c
Shota Morishita <webery.122@gmail.com>
2026-09-23 18:35:34 +0900

    feat: ラクするごはん LP 機能セクションの骨組みを追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import LpHeader from "@/features/lp/components/LpHeader";
 import HeroSection from "@/features/lp/components/HeroSection";
 import ProblemSection from "@/features/lp/components/ProblemSection";
 import CycleSection from "@/features/lp/components/CycleSection";
+import FeaturesSection from "@/features/lp/components/FeaturesSection";
 
 export default function IndexPage() {
   return (
@@ -11,6 +12,7 @@ export default function IndexPage() {
       <HeroSection />
       <ProblemSection />
       <CycleSection />
+      <FeaturesSection />
     </>
   );
 }
