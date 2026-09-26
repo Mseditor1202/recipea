1fc13b80c3d69b0c463e6e87a62dccbedce8048b
Shota Morishita <webery.122@gmail.com>
2026-09-26 17:18:41 +0900

    refactor: レシピ作成をAppLayoutへ移行

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -25,7 +25,8 @@ export default function App({ Component, pageProps }: AppProps) {
     router.pathname === "/home" ||
     router.pathname === "/recipes/weekly" ||
     router.pathname === "/recipes" ||
-    router.pathname === "/recipes/[id]";
+    router.pathname === "/recipes/[id]" ||
+    router.pathname === "/recipes/createpost";
 
   // LPと新Layoutページでは旧Navbarを表示しない
   const hideLegacyNavbar = isLp || usesAppLayout;
